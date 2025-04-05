7d84ced83139ecc5fcecefe87ed926a28487c0ec
hasan-mehboob <hassanmehboobwork@gmail.com>
2025-04-05 15:43:48 +0500

    solving issues in theme

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const FONT_FAMILY = "Montserrat, sans-serif";
+export const RECEIPT_FONT = "Arial, sans-serif";
+export const DRAWER_WIDTH = 340;
+export const DRAWER_HEAD_HEIGHT = 98;
+export const NAV_HEIGHT = 72;
+export const CLOSED_DRAWER_WIDTH = 65;
+export const BORDER_RADIUS = "5px";
